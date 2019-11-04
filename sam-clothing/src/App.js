8ee083bc687e1e9b9a8a65eb9f01c32708299300
Sam <Jeffcoat>
2019-11-04 17:27:09 -0600

    Buils homepage component and imports into our app

--- sam-clothing/src/App.js
+++ sam-clothing/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import HomePage from './homepage.component';
 function App() {
   return (
     <div>
-      <h1>Welcome To Sam's Clothing App</h1>
+      <HomePage />
     </div>
   );
 }
